e1d5aae7b5507e94000880ff94bebf90c9a0f48f
shawaa <andrew.shaw@mhr.co.uk>
2018-04-10 17:28:15 +0100

    init

---
+++ function.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+public static void Run(BrokeredMessage message, TraceWriter log)
+{
+    log.Info($"C# ServiceBus queue trigger function processed message: {message.MessageId}");
+    try
+    {
+        if (message != null)
+        {
+            //do something with the message here
+            log.Info("Completing message.");
+            message.Complete();
+        }
+    }
+    catch (Exception ex)
+    {
+        message.Abandon();
+        log.Info($"Exception occurred: {ex.Message}");
+    }
+}
