ca32ef2ff9641469d84a3d4afaf039c17dca4729
shawaa <andrew.shaw@mhr.co.uk>
2018-04-11 10:13:27 +0100

    bind to service bus

--- function.cs
+++ function.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.ServiceBus.Messaging;
+
 public static void Run(BrokeredMessage message, TraceWriter log)
 {
     log.Info($"C# ServiceBus queue trigger function processed message: {message.MessageId}");
