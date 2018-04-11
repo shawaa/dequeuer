9fc55f8c1c30d8255eb85c0f16bc8f6c8982c541
shawaa <andrew.shaw@mhr.co.uk>
2018-04-11 11:19:40 +0100

    refactor to attributes

--- function.cs
+++ function.cs
@@ -3,21 +3,11 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.ServiceBus.Messaging;
 
-public static void Run(BrokeredMessage message, TraceWriter log)
+[FunctionName("dequeuer")]                    
+public static void Run(
+    [ServiceBusTrigger("test1", Connection = "Endpoint=sb://function-dequeuer-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=tAITq/0PB5gvGS5FYcagimv/DgsKrvvYBqeT2J4DoEk=")] 
+    string myQueueItem, 
+    TraceWriter log)
 {
-    log.Info($"C# ServiceBus queue trigger function processed message: {message.MessageId}");
-    try
-    {
-        if (message != null)
-        {
-            //do something with the message here
-            log.Info("Completing message.");
-            message.Complete();
-        }
-    }
-    catch (Exception ex)
-    {
-        message.Abandon();
-        log.Info($"Exception occurred: {ex.Message}");
-    }
+    log.Info($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
 }
