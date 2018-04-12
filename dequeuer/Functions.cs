8ec7faf9a9d83c0b2f613b31fad5d27220a7800b
shawaa <andrew.shaw@mhr.co.uk>
2018-04-12 16:54:29 +0100

    use templated solution

---
+++ dequeuer/Functions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace dequeuer
+{
+    public static class Functions
+    {
+        [FunctionName("dequeuer")]
+        public static void Run(
+        [ServiceBusTrigger("test1", AccessRights.Manage, Connection = "sbConnection")]
+            string myQueueItem,
+            TraceWriter log)
+        {
+            log.Info($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
